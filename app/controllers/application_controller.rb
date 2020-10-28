f9604b02115fc84e3dc6cd587b1c31e37daf2fa1
Michael Kogan <kogan-misha@rambler.ru>
2020-10-28 18:17:40 +0300

    added comments edit helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :user_can_edit_comment?
+
+  def user_can_edit_comment?(comment)
+    if user_signed_in? && users_comment?(comment)
+      not_too_late_to_edit?(comment)
+    end
+  end
+
+  def users_comment?(comment)
+    comment.user == current_user
+  end
+
+  def not_too_late_to_edit?(comment)
+    comment.created_at > Time.now - 15.minutes
+  end
 end
