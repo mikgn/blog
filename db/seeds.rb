0c172a77820dc4c37a7d0c48c03efd2e0ab5f34b
Michael Kogan <kogan-misha@rambler.ru>
2020-10-28 18:18:50 +0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do |i|
+  user = User.create!(
+    name: Faker::Name.name ,
+    email: "user#{i+1}@test.com",
+    password: '123123'
+  )
+
+  10.times do
+    post = user.posts.create!(
+      title: Faker::Hipster.sentence,
+      body: Faker::Hipster.paragraph(sentence_count: rand(4..8)),
+      user_id: i
+    )
+  end
+end
+
+last_5_posts = Post.last(5)
+
+last_5_posts.each do |post|
+  10.times do
+    Comment.create!(
+      user: User.find(rand(1..10)),
+      post: post,
+      text: Faker::Hipster.paragraph(sentence_count: rand(4..8))
+    )
+  end
+end
+
+puts "100 users created"
+puts "1000 posts created"
+puts "comments created"
