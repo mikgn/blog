15ff52df29d5bec944f356b8b192b11f6f139b57
Michael Kogan <kogan-misha@rambler.ru>
2020-11-24 09:51:16 +0300

    refactored tags query

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,5 @@
 class TagsController < ApplicationController
   def show
-    @tag = Tag.find_by(name: params[:name])
-    @tag_posts = Post.joins(:tags).where(tags: { name: params[:name] })
+    @tag = Tag.joins(:posts).find_by_name(params[:name])
   end
 end
