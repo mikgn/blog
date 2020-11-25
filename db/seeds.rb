2e137c77bf8cf6e0bde1bd1d4f45598a0216c359
mikgn <kogan.miha@gmail.com>
2020-11-25 14:11:40 +0300

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,62 @@
-100.times do |i|
+hashtags = [
+  '#love', '#instagood', '#photooftheday', '#fashion',
+  '#beautiful', '#happy', '#cute', '#tbt',
+  '#like4like', '#followme', '#picoftheday', '#follow',
+  '#me', '#selfie', '#summer', '#art',
+  '#instadaily', '#friends', '#repost', '#nature',
+  '#girl', '#fun', '#style', '#smile',
+  '#food', '#instalike', '#likeforlike', '#family',
+  '#travel', '#fitness', '#igers', '#tagsforlikes',
+  '#follow4follow', '#nofilter', '#life', '#beauty',
+  '#amazing', '#instamood', '#instagram', '#photography'
+]
+
+10_000.times do |i|
   user = User.create!(
-    name: Faker::Name.name ,
+    name: "#{Faker::Name.name}#{i}",
     email: "user#{i+1}@test.com",
     password: '123123'
   )
 
-  10.times do
+  5.times do
     post = user.posts.create!(
       title: Faker::Hipster.sentence,
       body: Faker::Hipster.paragraph(sentence_count: rand(4..8)),
       user_id: i
     )
   end
-end
 
-last_5_posts = Post.last(5)
+  # posts with hashtag
+  3.times do
+    post = user.posts.create!(
+      title: Faker::Hipster.sentence,
+      body: "#{hashtags.sample } " + Faker::Hipster.paragraph(sentence_count: rand(4..8)),
+      user_id: i
+    )
+  end
 
-last_5_posts.each do |post|
-  10.times do
-    Comment.create!(
-      user: User.find(rand(1..10)),
-      post: post,
-      text: Faker::Hipster.paragraph(sentence_count: rand(4..8))
+  # posts with 2 hashtags
+  2.times do
+    post = user.posts.create!(
+      title: Faker::Hipster.sentence,
+      body: "#{hashtags.sample } " + "#{hashtags.sample } " + Faker::Hipster.paragraph(sentence_count: rand(4..8)),
+      user_id: i
     )
   end
 end
 
+puts "10_000 users created"
+puts "100_000 posts with hashtags created"
 
-puts "100 users created"
-puts "1000 posts created"
-puts "comments created"
+users = User.all
+posts = Post.all
 
-# hashtag checking
+500_000.times do
+  Comment.create(
+    text: Faker::Hipster.paragraph(sentence_count: rand(2..6)),
+    user: users.sample,
+    post: posts.sample
+  )
+end
 
-Post.create!(
-  title: '#hashtag title',
-  body: '#Lorem Ipsum is #simply dummy #text of the #printing and typesetting #industry',
-  user_id: 1
-)
-
-Post.create!(
-  title: 'hashtag #title',
-  body: 'Lorem #Ipsum is simply #dummy text of #the printing and #typesetting industry',
-  user_id: 1
-)
-
-puts '2 post with hashtags created'
+puts "500_000 comments created"
